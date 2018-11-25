10a12e841768467661d73ccc082283063e1f9c9e
Ignacio Martinez Vazquez <ignamv@gmail.com>
2018-11-25 10:17:50 -0300

    make.sh y svgs.sh

--- tesis/presentacion/make.sh
+++ tesis/presentacion/make.sh
@@ -4,7 +4,14 @@ pandoc presentacion.md -o presentacion.html \
     -t revealjs -s --from markdown --slide-level=2 \
     -V theme=beige --css tipi.css \
     --mathjax=MathJax/MathJax.js
-pandoc presentacion.md -o presentacion2.html -s --from markdown \
-    --mathjax=MathJax/MathJax.js --css tipi.css
+
+#pandoc presentacion.md -o presentacion.pptx \
+    #-s --from markdown --slide-level=2 
+
+#pandoc presentacion.md -o presentacion.pdf \
+    #-s --from markdown --slide-level=2 -t beamer --pdf-engine=xelatex
+
+#pandoc presentacion.md -o presentacion2.html -s --from markdown \
+    #--mathjax=MathJax/MathJax.js --css tipi.css
 
 echo \# Slides: `grep '\#\#' presentacion.md | wc -l`
--- tesis/presentacion/svgs.sh
+++ tesis/presentacion/svgs.sh
@@ -20,3 +20,9 @@ pdf2svg ../figuras/fg/irradiacion_sensibilidad.pdf figuras/fg_sr90_sensibilidad.
 pdf2svg ../figuras/fg/ruido.pdf figuras/fg_ruido.svg
 pdf2svg ../figuras/fg/esquemainyeccion.pdf figuras/inyeccion.svg
 pdf2svg ../figuras/fg/irradiacion.pdf figuras/irradiacion_fg.svg
+
+cp ../figuras/radmos/radiacion_mos.svg figuras/radiacion_mos.svg
+cp ../figuras/poster/posicion_no.png figuras/posicion_no.png
+cp ../figuras/poster/posicion_si.png figuras/posicion_si.png
+cp ../figuras/irradiador/corte_esfera.png figuras/corte_esfera.png
+cp ../figuras/instrumental/guarded.png figuras/guarded.png
